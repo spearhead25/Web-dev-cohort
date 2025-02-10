55e26da513fa8018f0ce9cd1e494f78427289e7c
Harshul Malik <harshulmalik5@gmail.com>
2025-02-10 21:25:27 +0530

    Started with JS

---
+++ Week1-Week3/Basics.js
@@ -0,0 +1,19 @@
+console.log("Hello World");
+
+function start(){
+    console.log("Hello Humans");
+}
+
+function bringBrush(howmuch){
+    console.log("Le aao brush", howmuch);
+    console.log(`Leke aa rahe ${howmuch} brush`);
+}
+
+function addTwo(num1, num2){
+    return num1 + num2;
+}
+
+start();
+bringBrush(4);
+addTwo(1,1);
+console.log(addTwo(2,2));
