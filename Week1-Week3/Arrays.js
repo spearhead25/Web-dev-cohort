d0a09989c554209c630399df2c09cd1a10271741
Harshul Malik <harshulmalik5@gmail.com>
2025-02-11 13:36:00 +0530

    Completed Loops in JS

---
+++ Week1-Week3/Arrays.js
@@ -0,0 +1,9 @@
+let fruits = ["apple", "cherry", "banana"];
+let intFruits = new Array("kiwi", "avacado", "dragon fruit");
+
+fruits[0] = "Grapes";
+fruits.push("Guava");
+fruits.unshift("Mango");
+
+console.log(fruits);
+console.log(intFruits);
---
+++ Week1-Week3/Loop.js
@@ -0,0 +1,7 @@
+let teas = ["masala", "ginger", "oolong", "orange", "rose", "lemon"]
+
+teas.length;
+
+for(let h=0; h<teas.length; h++){
+    console.log(`tea at index ${h}: ${teas[h]}`);
+}
