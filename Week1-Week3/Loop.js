e34d736e17109a2d9561a125cd2a5331b038aba2
Harshul Malik <harshulmalik5@gmail.com>
2025-02-11 20:40:06 +0530

    Dne with loops

--- Week1-Week3/Loop.js
+++ Week1-Week3/Loop.js
@@ -5,3 +5,17 @@ teas.length;
 for(let h=0; h<teas.length; h++){
     console.log(`tea at index ${h}: ${teas[h]}`);
 }
+
+
+let myArray = [1, 4, 2, 3, 5, 6];
+
+function sumfac(numbers){
+    let sum = 0;
+    for(let i=0; i<numbers.length; i++){
+        sum = sum + numbers[i];
+    }
+    return sum;
+}
+
+let result = sumfac(myArray);
+console.log(result);
