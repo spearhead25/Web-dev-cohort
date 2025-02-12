005edb337bece348c806e21077cb351eff1fa083
Harshul Malik <harshulmalik5@gmail.com>
2025-02-12 14:16:13 +0530

    Completed week3 with some basics of JavaScript

--- Week1-Week3/Arrays.js
+++ Week1-Week3/Arrays.js
@@ -7,3 +7,52 @@ fruits.unshift("Mango");
 
 console.log(fruits);
 console.log(intFruits);
+
+const teas = ["Green tea", "Black Tea", "Oolong Tea", "White Tea", "Herbal Tea"];
+console.log(teas);
+
+teas.push("Chamomile Tea");
+let peas = teas.filter(item => item !== "Oolong Tea");
+console.log(peas);
+
+let index = teas.indexOf("Oolong Tea");
+if(index !== -1){
+    teas.splice(index,1);
+}
+console.log(teas);
+
+console.log(teas.sort());
+
+for(let i=0; i<teas.length; i++){
+    console.log(teas[i]);
+}
+
+let count = 0;
+for(let i=0; i<teas.length; i++){
+     if(teas[i] !== "Herbal Tea"){
+        count++;
+     } 
+}
+console.log(count);
+
+let uppercaseteas = teas.map(tea => tea.toUpperCase());
+console.log(uppercaseteas);
+
+let longestTea = "";
+for(let i=0; i<teas.length; i++){
+    if(teas[i].length > longestTea.length){
+        longestTea = teas[i];
+    }
+}
+console.log(longestTea);
+
+let reverse = [];
+for(let i=teas.length - 1; i>=0; i--){
+    reverse.push(teas[i]);
+}
+console.log(reverse);
+
+
+
+
+
