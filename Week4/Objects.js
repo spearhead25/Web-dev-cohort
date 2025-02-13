05603db7b36e58a6f45229b02a4e577052cf71a1
Harshul Malik <harshulmalik5@gmail.com>
2025-02-13 17:08:19 +0530

    Basics of objects

---
+++ Week4/Objects.js
@@ -0,0 +1,34 @@
+const person = {
+    x: 10,
+    firstName: "Harshul",
+    lastName: "Malik",
+    address: {
+        hno: 1,
+        street: 1,
+        countryCode: 'IN',
+    }
+}
+
+console.log(person.x);
+
+const remote = {
+    color: 'black',
+    brand: 'XYZ',
+    dimensions: {height: 1, width: 1},
+    turnOff: function() {
+
+    },
+    volumeUp: function(){
+
+    }
+}
+
+let p1 = {
+    fname: "harry"
+}
+
+let p2 = p1;
+p2.fname = "Parul";
+
+console.log(p2);
+console.log(p1);
